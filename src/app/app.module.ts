a13cf466005642742774d8a0070fc6ef44877540
G3TLeighton <gwl@g3t.uk.com>
2019-08-27 09:12:18 +0100

    material css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TaskListComponent } from './components/TaskList/TaskList.component'
-import { TaskComponent } from './components/TaskList/Task/Task.component'
-import { NewTaskComponent } from './components/TaskList/NewTask/NewTask.component';
-import { CheckBoxComponent } from './components/ui/check-box/check-box.component';
-import { ToggleComponent } from './components/ui/toggle/toggle.component';
-import { ButtonComponent } from './components/ui/button/button.component'
+
 
 
 
@@ -16,12 +11,7 @@ import { ButtonComponent } from './components/ui/button/button.component'
 @NgModule({
   declarations: [
     AppComponent,
-    TaskListComponent,
-    TaskComponent,
-    NewTaskComponent,
-    CheckBoxComponent,
-    ToggleComponent,
-    ButtonComponent
+   
    
   ],
   imports: [
